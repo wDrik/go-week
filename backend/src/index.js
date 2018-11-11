cd56122efab6378a1db1ff746a50036c2bb92aa9
Iorgen <w.drik@outlook.com>
2018-11-11 13:10:29 -0200

    Conection mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/goweek', {
+  useNewUrlParser: true
+})
+
 app.get('/', (req, res) => {
-  return res.send('Hello World!');
+  return res.send('Hello World!!!');
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log(':) Server started on port 3000');
 });
